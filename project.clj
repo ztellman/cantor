c804d8057d2b6c8a75b91dd03d64c8d74d3ab230
prospero <ztellman@gmail.com>
2010-05-01 16:53:02 -0700

    master is at 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cantor "0.1.0"
+(defproject cantor "0.2.0-SNAPSHOT"
   :description "primitive math for clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
