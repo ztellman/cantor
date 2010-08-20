144ad4b849325cb8388c3e832ca826a939b8baa9
prospero <ztellman@gmail.com>
2010-08-19 21:02:02 -0700

    update targeted clojure version to 1.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cantor "0.3.0-SNAPSHOT"
   :description "primitive math for clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [leiningen/lein-swank "1.2.0-SNAPSHOT"]
 				     [autodoc "0.7.1"]])
