7c28c273ec9e9e0c5b09013ac9a0a6c53e85eddb
ztellman <ztellman@gmail.com>
2011-06-11 17:09:14 -0700

    take out unnecessary dependencies

--- project.clj
+++ project.clj
@@ -2,6 +2,4 @@
   :description "primitive math for clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [leiningen/lein-swank "1.2.0-SNAPSHOT"]
-				     [autodoc "0.7.1"]])
+  :dev-dependencies [])
